d856dbb13862e3bbc8527d7a6dab7bd9b7985408
Fabian Barreto <fdbarretos@correo.udistrital.edu.co>
2019-09-21 20:38:48 -0500

    Se desarrolla consulta general de los proyectos academicos #17

--- src/app/pages/proyecto_academico/list-proyecto_academico/list-proyecto_academico.component.ts
+++ src/app/pages/proyecto_academico/list-proyecto_academico/list-proyecto_academico.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { LocalDataSource } from 'ng2-smart-table';
 import { CampusMidService } from '../../../@core/data/campus_mid.service';
 import { UserService } from '../../../@core/data/users.service';
@@ -9,6 +9,15 @@ import { ProduccionAcademicaPost } from '../../../@core/data/models/produccion_a
 import { HttpErrorResponse } from '@angular/common/http';
 import Swal from 'sweetalert2';
 import 'style-loader!angular2-toaster/toaster.css';
+import { MatTableDataSource } from '@angular/material';
+import { ProyectoAcademicoService } from '../../../@core/data/proyecto_academico.service';
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
 
 @Component({
   selector: 'ngx-list-proyecto-academico',
@@ -16,146 +25,104 @@ import 'style-loader!angular2-toaster/toaster.css';
   styleUrls: ['./list-proyecto_academico.component.scss'],
   })
 export class ListProyectoAcademicoComponent implements OnInit {
-  prod_selected: ProduccionAcademicaPost;
   cambiotab: boolean = false;
   config: ToasterConfig;
   settings: any;
+  dataSource: any;
+  displayedColumns = ['Id', 'Facultad', 'Nombre Proyecto', 'Nivel Proyecto', 'codigo', 'Activo', 'registro', 'calidad', 'Consulta', 'editar', 'inhabilitar'];
 
   source: LocalDataSource = new LocalDataSource();
 
   constructor(private translate: TranslateService,
     private campusMidService: CampusMidService,
     private user: UserService,
+    private proyectoacademicoService: ProyectoAcademicoService,
     private toasterService: ToasterService) {
+      this.loadproyectos();
   }
 
-  cargarCampos() {
-    this.settings = {
-      add: {
-        addButtonContent: '<i class="nb-plus"></i>',
-        createButtonContent: '<i class="nb-checkmark"></i>',
-        cancelButtonContent: '<i class="nb-close"></i>',
-      },
-      edit: {
-        editButtonContent: '<i class="nb-edit"></i>',
-        saveButtonContent: '<i class="nb-checkmark"></i>',
-        cancelButtonContent: '<i class="nb-close"></i>',
-      },
-      delete: {
-        deleteButtonContent: '<i class="nb-trash"></i>',
-        confirmDelete: true,
-      },
-      mode: 'external',
-      columns: {
-        // Persona: {
-        //   title: this.translate.instant('GLOBAL.persona'),
-        //   // type: 'number;',
-        //   valuePrepareFunction: (value) => {
-        //     return value;
-        //   },
-        // },
-        Titulo: {
-          title: this.translate.instant('produccion_academica.titulo_produccion_academica'),
-          // type: 'string;',
-          valuePrepareFunction: (value) => {
-            return value;
-          },
-          width: '20%',
-        },
-        SubtipoProduccionId: {
-          title: this.translate.instant('produccion_academica.tipo_produccion_academica'),
-          // type: 'tipo_produccion_academica;',
-          valuePrepareFunction: (value) => {
-            return value.Nombre;
-          },
-          width: '15%',
-        },
-        Resumen: {
-          title: this.translate.instant('produccion_academica.resumen'),
-          // type: 'string;',
-          valuePrepareFunction: (value) => {
-            return value;
-          },
-          width: '30%',
-        },
-        EstadoEnteAutorId: {
-          title: this.translate.instant('produccion_academica.estado_autor'),
-          // type: 'string',
-          valuePrepareFunction: (value) => {
-            return value.EstadoAutorProduccionId.Nombre;
-          },
-          width: '15%',
-        },
-        Fecha: {
-          title: this.translate.instant('produccion_academica.fecha_publicacion'),
-          // type: 'string;',
-          valuePrepareFunction: (value) => {
-            return ((value) + '').substring(0, 10);
-          },
-          width: '10%',
-        },
-      },
-    };
+  applyFilter(filterValue: string) {
+    this.proyectoacademicoService.get('registro_calificado_acreditacion/' )
+    .subscribe(res => {
+    const r = <any>res;
+    if (res !== null && r.Type !== 'error') {
+      this.dataSource = new MatTableDataSource();
+      this.dataSource = res;
+      this.dataSource = this.dataSource.filter((row: any) => (((row.ProyectoAcademicoInstitucionId.Nombre )
+      .toLowerCase()).indexOf(filterValue.toLowerCase())) !== -1)
   }
+},
+(error: HttpErrorResponse) => {
+  Swal({
+    type: 'error',
+    title: error.status + '',
+    text: this.translate.instant('ERROR.' + error.status),
+    confirmButtonText: this.translate.instant('GLOBAL.aceptar'),
+  });
+});
+}
 
   useLanguage(language: string) {
     this.translate.use(language);
   }
 
-  loadData(): void {
-    this.campusMidService.get('produccion_academica/' + this.user.getPersonaId()).subscribe((res: any) => {
-    // this.campusMidService.get('produccion_academica/' + 5).subscribe((res: any) => {
-      if (res !== null) {
-        if (Object.keys(res.Body[0]).length > 0 && res.Type !== 'error') {
-          const data = <Array<ProduccionAcademicaPost>>res.Body;
-          this.source.load(data);
-        } else {
-           Swal({
-            type: 'error',
-            title: '404',
-            text: this.translate.instant('ERROR.404'),
-            confirmButtonText: this.translate.instant('GLOBAL.aceptar'),
-          });
-        }
-      }
-    }, (error: HttpErrorResponse) => {
-      Swal({
-        type: 'error',
-        title: error.status + '',
-        text: this.translate.instant('ERROR.' + error.status),
-        confirmButtonText: this.translate.instant('GLOBAL.aceptar'),
-      });
-    });
-  }
-
   ngOnInit() {
+
   }
-
-
-
-  activetab(): void {
-    this.cambiotab = !this.cambiotab;
-  }
-
-  selectTab(event): void {
-    if (event.tabTitle === this.translate.instant('GLOBAL.lista')) {
-      this.cambiotab = false;
-    } else {
-      this.cambiotab = true;
+  loadproyectos() {
+    const opt1: any = {
+      title: this.translate.instant('GLOBAL.atencion'),
+      text: this.translate.instant('oferta.evento'),
+      icon: 'warning',
+      buttons: true,
+      dangerMode: true,
+      showCancelButton: true,
     }
-  }
-
-  onChange(event) {
-    if (event) {
-      this.loadData();
-      this.cambiotab = !this.cambiotab;
+    this.proyectoacademicoService.get('registro_calificado_acreditacion/' )
+    .subscribe(res => {
+    if (res !== null && res[0] !== 'error') {
+      this.dataSource = new MatTableDataSource();
+      console.info(res)
+      this.dataSource = res
+    }else {
+      Swal(opt1)
+      .then((willDelete) => {
+        if (willDelete.value) {
+        }
+      });
     }
+  },
+  (error: HttpErrorResponse) => {
+    Swal({
+      type: 'error',
+      title: error.status + '',
+      text: this.translate.instant('ERROR.' + error.status),
+      confirmButtonText: this.translate.instant('GLOBAL.aceptar'),
+    });
+  });
   }
 
-
-  itemselec(event): void {
-    // console.log("afssaf");
+  private showToast(type: string, title: string, body: string) {
+    this.config = new ToasterConfig({
+      // 'toast-top-full-width', 'toast-bottom-full-width', 'toast-top-left', 'toast-top-center'
+      positionClass: 'toast-top-center',
+      timeout: 5000,  // ms
+      newestOnTop: true,
+      tapToDismiss: false, // hide on click
+      preventDuplicates: true,
+      animation: 'slideDown', // 'fade', 'flyLeft', 'flyRight', 'slideDown', 'slideUp'
+      limit: 5,
+    });
+    const toast: Toast = {
+      type: type, // 'default', 'info', 'success', 'warning', 'error'
+      title: title,
+      body: body,
+      showCloseButton: true,
+      bodyOutputType: BodyOutputType.TrustedHtml,
+    };
+    this.toasterService.popAsync(toast);
   }
+
 
 
 }
