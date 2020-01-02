d6904b197d733174e0f5b72be9e67248494c34b3
JuanPiedrahita <juangonzalez1597@gmail.com>
2020-01-02 14:00:14 -0500

    #134 lint

--- src/app/pages/propuesta_grado/crud-propuesta_grado/crud-propuesta_grado.component.ts
+++ src/app/pages/propuesta_grado/crud-propuesta_grado/crud-propuesta_grado.component.ts
@@ -339,7 +339,6 @@ export class CrudPropuestaGradoComponent implements OnInit {
                       });
                     });
                 }
-                
               },
                 (error: HttpErrorResponse) => {
                   Swal({
