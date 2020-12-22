1726c27234fa5c077dc13dffa29aadf202998254
Ricardo Avendaño Casas <ricardoavendanocasas@MacBook-Pro-de-Ricardo.local>
2020-12-22 04:18:51 -0500

    fix: creacion modulo gestion tipos de inscripcion

---
+++ src/app/pages/tipo_inscripcion/tipo_inscripcion.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-tipo-inscripcion-elements',
+  template: `<router-outlet></router-outlet>`,
+})
+export class TipoInscripcionComponent {
+}
