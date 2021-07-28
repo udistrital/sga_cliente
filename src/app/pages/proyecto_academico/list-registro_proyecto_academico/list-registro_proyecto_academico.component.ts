f55bc1118e348bdd89fb56886039c0e7996ebbe3
JQuinteroC <jolquinteroc@correo.udistrital.edu.co>
2021-07-28 18:15:51 -0500

    refactor: se ajusto la tabla a ng2-smart-table. #561

--- src/app/pages/proyecto_academico/list-registro_proyecto_academico/list-registro_proyecto_academico.component.ts
+++ src/app/pages/proyecto_academico/list-registro_proyecto_academico/list-registro_proyecto_academico.component.ts
@@ -7,9 +7,10 @@ import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
 // import { UserService } from '../../../@core/data/users.service';
 import { ProduccionAcademicaPost } from '../../../@core/data/models/produccion_academica/produccion_academica';
 import { HttpErrorResponse } from '@angular/common/http';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import Swal from 'sweetalert2';
 import 'style-loader!angular2-toaster/toaster.css';
+import * as momentTimezone from 'moment-timezone';
 import { MatTableDataSource } from '@angular/material';
 import { Inject } from '@angular/core';
 import { ProyectoAcademicoService } from '../../../@core/data/proyecto_academico.service';
@@ -28,7 +29,7 @@ import { DocumentoService } from '../../../@core/data/documento.service';
   selector: 'ngx-list-registro-proyecto-academico',
   templateUrl: './list-registro_proyecto_academico.component.html',
   styleUrls: ['./list-registro_proyecto_academico.component.scss'],
-  })
+})
 export class ListRegistroProyectoAcademicoComponent implements OnInit {
   config: ToasterConfig;
   settings: any;
@@ -36,9 +37,10 @@ export class ListRegistroProyectoAcademicoComponent implements OnInit {
   index: any;
   idproyecto: any;
   displayedColumns = ['Id', 'Registro', 'Vigencia', 'Tipo de registro', 'Documento', 'Tiempo de vigencia', 'Activo',
-  'Fecha Inicio Registro', 'Fecha Vencimiento Registro'];
+    'Fecha Inicio Registro', 'Fecha Vencimiento Registro'];
 
   source: LocalDataSource = new LocalDataSource();
+  listaDatos = [];
   proyectoJson: any;
 
   constructor(private translate: TranslateService,
@@ -50,22 +52,135 @@ export class ListRegistroProyectoAcademicoComponent implements OnInit {
     private documentoService: DocumentoService,
     public dialog: MatDialog,
     private toasterService: ToasterService) {
-      this.loadregistro();
+    this.loadregistro();
+    this.loadData();
+    this.cargarCampos();
+    this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
+      this.cargarCampos();
+    });
+  }
+
+  cargarCampos() {
+    this.settings = {
+      columns: {
+        Id: {
+          title: this.translate.instant('consultaproyecto.id'),
+          width: '5%',
+        },
+        NumeroActoAdministrativo: {
+          title: this.translate.instant('historial_registro.registro'),
+          width: '5%',
+        },
+        AnoActoAdministrativoId: {
+          title: this.translate.instant('historial_registro.vigencia'),
+          width: '5%',
+        },
+        TipoRegistroIdNombre: {
+          title: this.translate.instant('historial_registro.tipo_registro'),
+          width: '25%',
+        },
+        VigenciaActoAdministrativo: {
+          title: this.translate.instant('historial_registro.tiempo'),
+          width: '10%',
+        },
+        ActivoLetra: {
+          title: this.translate.instant('historial_registro.activo'),
+          width: '5%',
+        },
+        FechaCreacionActoAdministrativo: {
+          title: this.translate.instant('historial_registro.fecha_inicio'),
+          width: '15%',
+        },
+        VencimientoActoAdministrativo: {
+          title: this.translate.instant('historial_registro.fecha_vencimiento'),
+          width: '18%',
+        },
+      },
+      mode: 'external',
+      actions: {
+        add: false,
+        edit: false,
+        delete: false,
+        position: 'right',
+        columnTitle: this.translate.instant('historial_registro.documento'),
+        custom: [
+          {
+            name: 'Documento',
+            title: '<i class="fa fa-cloud-download" title="' +
+              this.translate.instant('GLOBAL.tooltip_descargar') +
+              '"></i>',
+          },
+        ],
+      },
+    };
+    //this.source.load(this.listaDatos);
+  }
+
+  loadData(): void {
+    const opt1: any = {
+      title: this.translate.instant('GLOBAL.atencion'),
+      text: this.translate.instant('oferta.evento'),
+      icon: 'warning',
+      buttons: true,
+      dangerMode: true,
+      showCancelButton: true,
+    }
+    this.sgamidService.get('consulta_proyecto_academico/get_registro/' + this.data.Id)
+      .subscribe(res => {
+        if (res !== null && res[0] !== 'error') {
+          const data = <Array<any>>res;
+          data.forEach(element => {
+            element.TipoRegistroIdNombre = element.TipoRegistroId.Nombre
+            element.FechaCreacionActoAdministrativo = momentTimezone.tz(element.FechaCreacionActoAdministrativo, 'America/Bogota').format('DD-MM-YYYY')
+            element.VencimientoActoAdministrativo = momentTimezone.tz(element.VencimientoActoAdministrativo, 'America/Bogota').format('DD-MM-YYYY')
+            this.source.load(data)
+          });
+        } else {
+          Swal.fire(opt1)
+            .then((willDelete) => {
+              if (willDelete.value) {
+              }
+            });
+        }
+      },
+        (error: HttpErrorResponse) => {
+          Swal.fire({
+            icon: 'error',
+            title: error.status + '',
+            text: this.translate.instant('ERROR.' + error.status),
+            confirmButtonText: this.translate.instant('GLOBAL.aceptar'),
+          });
+        });
+  }
+
+  onAction(event) {
+    console.log("Se llamo al botón")
+
+    this.highlight(event)
+    console.log(event)
+    switch (event.action) {
+      case 'Documento':
+        this.downloadFile(event);
+        break;
+    }
   }
 
   downloadFile(id_documento: any) {
-    const filesToGet = [
+    console.log("Se llamo a download")
+    let filesToGet = [
       {
-        Id: id_documento,
-        key: id_documento,
+        Id: id_documento.data.EnlaceActo,
+        key: id_documento.data.EnlaceActo,
       },
     ];
     this.nuxeoService.getDocumentoById$(filesToGet, this.documentoService)
       .subscribe(response => {
+        console.log(response)
         const filesResponse = <any>response;
         if (Object.keys(filesResponse).length === filesToGet.length) {
           // console.log("files", filesResponse);
           filesToGet.forEach((file: any) => {
+            console.log("--", file)
             const url = filesResponse[file.Id];
             window.open(url);
           });
@@ -73,7 +188,7 @@ export class ListRegistroProyectoAcademicoComponent implements OnInit {
       },
       (error: HttpErrorResponse) => {
         Swal.fire({
-          icon:'error',
+          icon: 'error',
           title: error.status + '',
           text: this.translate.instant('ERROR.' + error.status),
           confirmButtonText: this.translate.instant('GLOBAL.aceptar'),
@@ -92,67 +207,66 @@ export class ListRegistroProyectoAcademicoComponent implements OnInit {
     this.dialogRef.close();
   }
 
-  highlight(row ): void {
-    this.idproyecto = row.ProyectoAcademicoInstitucionId.Id;
-
- }
- loadregistro() {
-  const opt1: any = {
-    title: this.translate.instant('GLOBAL.atencion'),
-    text: this.translate.instant('oferta.evento'),
-    icon: 'warning',
-    buttons: true,
-    dangerMode: true,
-    showCancelButton: true,
+  highlight(row): void {
+    this.idproyecto = row.data.ProyectoAcademicoInstitucionId.Id;
   }
-  this.sgamidService.get('consulta_proyecto_academico/get_registro/' + this.data.Id )
-  .subscribe(res => {
-  if (res !== null && res[0] !== 'error') {
-    this.dataSource = new MatTableDataSource();
-    this.dataSource = res
-  }else {
-    Swal.fire(opt1)
-    .then((willDelete) => {
-      if (willDelete.value) {
-      }
+
+  loadregistro() {
+    const opt1: any = {
+      title: this.translate.instant('GLOBAL.atencion'),
+      text: this.translate.instant('oferta.evento'),
+      icon: 'warning',
+      buttons: true,
+      dangerMode: true,
+      showCancelButton: true,
+    }
+    console.log(this.data.Id)
+    this.sgamidService.get('consulta_proyecto_academico/get_registro/' + this.data.Id)
+      .subscribe(res => {
+        if (res !== null && res[0] !== 'error') {
+          this.dataSource = new MatTableDataSource();
+          this.dataSource = res
+        } else {
+          Swal.fire(opt1)
+            .then((willDelete) => {
+              if (willDelete.value) {
+              }
+            });
+        }
+      },
+        (error: HttpErrorResponse) => {
+          Swal.fire({
+            icon: 'error',
+            title: error.status + '',
+            text: this.translate.instant('ERROR.' + error.status),
+            confirmButtonText: this.translate.instant('GLOBAL.aceptar'),
+          });
+        });
+  }
+
+  OpenRegistroCalificado(): void {
+    const dialogRef = this.dialog.open(RegistroProyectoAcademicoComponent, {
+      width: '550px',
+      height: '750px',
+      data: { IdProyecto: this.data.Id, endpoint: 'registro_calificado', tipo_registro: 1 },
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.loadregistro();
     });
   }
-},
-(error: HttpErrorResponse) => {
-  Swal.fire({
-    icon:'error',
-    title: error.status + '',
-    text: this.translate.instant('ERROR.' + error.status),
-    confirmButtonText: this.translate.instant('GLOBAL.aceptar'),
-  });
-});
-}
 
-OpenRegistroCalificado(): void {
-  const dialogRef = this.dialog.open(RegistroProyectoAcademicoComponent, {
-    width: '550px',
-    height: '750px',
-    data: { IdProyecto: this.data.Id, endpoint: 'registro_calificado', tipo_registro: 1},
-  });
+  OpenRegistroAlta(): void {
+    const dialogRef = this.dialog.open(RegistroProyectoAcademicoComponent, {
+      width: '550px',
+      height: '750px',
+      data: { IdProyecto: this.data.Id, endpoint: 'registro_alta_calidad', tipo_registro: 2 },
+    });
 
-  dialogRef.afterClosed().subscribe(result => {
-    this.loadregistro();
-  });
-}
-
-
-OpenRegistroAlta(): void {
-  const dialogRef = this.dialog.open(RegistroProyectoAcademicoComponent, {
-    width: '550px',
-    height: '750px',
-    data: { IdProyecto: this.data.Id, endpoint: 'registro_alta_calidad', tipo_registro: 2},
-  });
-
-  dialogRef.afterClosed().subscribe(result => {
-    this.loadregistro();
-  });
-}
-
+    dialogRef.afterClosed().subscribe(result => {
+      this.loadregistro();
+    });
+  }
 
   private showToast(type: string, title: string, body: string) {
     this.config = new ToasterConfig({
@@ -174,7 +288,4 @@ OpenRegistroAlta(): void {
     };
     this.toasterService.popAsync(toast);
   }
-
-
-
 }
