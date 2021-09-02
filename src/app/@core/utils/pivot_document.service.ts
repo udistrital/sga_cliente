6fd031d51e31dbb1e731748d440ec75785a748cf
Fabián Sánchez <fadarsaleeing@gmail.com>
2021-09-02 17:47:21 -0500

    fix: solve viewDescuento, update flow without sessionStorage #632

--- src/app/@core/utils/pivot_document.service.ts
+++ src/app/@core/utils/pivot_document.service.ts
@@ -10,7 +10,14 @@ export class PivotDocument {
     private documentSubject = new BehaviorSubject(null);
     public document$ = this.documentSubject.asObservable();
 
+    private infoSubject = new BehaviorSubject(null);
+    public info$ = this.infoSubject.asObservable();
+
     constructor() {
+    }
+
+    updateInfo(info){
+        this.infoSubject.next(info);
     }
 
     updateDocument(document) {
