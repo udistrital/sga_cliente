3ed7eb349a12838efc4f7e14aa397f691e3c411d
Fabián Sánchez <fadarsaleeing@gmail.com>
2022-01-21 13:13:32 -0500

    componente de notas #818

--- src/app/pages/registro_notas/crud-notas/crud-notas.component.ts
+++ src/app/pages/registro_notas/crud-notas/crud-notas.component.ts
@@ -9,6 +9,16 @@ import { FormGroup } from '@angular/forms';
 export class CrudNotasComponent implements OnInit {
   totalPercentage = 0
   parcialPercentage = [0, 0, 0]
+
+  students = [
+    { name: 'Pedro1', code: 99999, lastname: 'Sánchez1' },
+    { name: 'Pedro2', code: 99999, lastname: 'Sánchez2' },
+    { name: 'Pedro3', code: 99999, lastname: 'Sánchez3' },
+    { name: 'Pedro4', code: 99999, lastname: 'Sánchez4' },
+    { name: 'Pedro5', code: 99999, lastname: 'Sánchez5' },
+    { name: 'Pedro6', code: 99999, lastname: 'Sánchez6' },
+    { name: 'Pedro7', code: 99999, lastname: 'Sánchez7' },
+  ]
   settings1 = {
     name: 'Corte 1',
     percentage: 35,
@@ -18,6 +28,11 @@ export class CrudNotasComponent implements OnInit {
       { placeholder: '% P2', label: 'P2', name: 'p2' },
       { placeholder: '% P3', label: 'P3', name: 'p3' },
     ]
+  }
+
+  notes1 = {
+    fields :this.settings1.fields,
+    type: 'notes',
   }
 
 
@@ -33,6 +48,11 @@ export class CrudNotasComponent implements OnInit {
     ]
   }
 
+  notes2 = {
+    fields :this.settings2.fields,
+    type: 'notes',
+  }
+
   settings3 = {
     name: 'Examen final',
     type: 'percentage',
@@ -40,6 +60,11 @@ export class CrudNotasComponent implements OnInit {
     fields: [
       { placeholder: '% P1', label: 'P1', name: 'p1', value: 30 },
     ]
+  }
+
+  notes3 = {
+    fields :this.settings3.fields,
+    type: 'notes',
   }
 
   settings4 = {
@@ -50,6 +75,12 @@ export class CrudNotasComponent implements OnInit {
       { placeholder: '% P1', label: 'P1', name: 'p1', value: 70 },
     ]
   }
+
+  notes4 = {
+    fields :this.settings4.fields,
+    type: 'notes',
+  }
+
   constructor() { }
 
   ngOnInit() {
@@ -93,5 +124,9 @@ export class CrudNotasComponent implements OnInit {
     const { percentages } = value;
     this.parcialPercentage[index] = percentages.map(a => a.field).reduce((a, b) => a + b);
     this.totalPercentage = this.parcialPercentage.reduce((a, b) => a + b)
+  }
+
+  formNotes() {
+    
   }
 }
