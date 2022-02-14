2157551bbb6180195832f30e0318c809e11fb22e
JQuinteroC <jolquinteroc@correo.udistrital.edu.co>
2022-02-13 19:22:05 -0500

    feat: Ajustes para la carga de transferencias y reintegros junto a su información. #859

--- src/app/pages/inscripcion/solicitud-transferencia/solicitud-transferencia.component.ts
+++ src/app/pages/inscripcion/solicitud-transferencia/solicitud-transferencia.component.ts
@@ -4,6 +4,8 @@ import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 import { UtilidadesService } from '../../../@core/utils/utilidades.service';
 import { FORM_SOLICITUD_TRANSFERENCIA, FORM_RESPUESTA_SOLICITUD } from '../forms-transferencia';
 import { ActivatedRoute } from '@angular/router';
+import { SgaMidService } from '../../../@core/data/sga_mid.service';
+import { PopUpManager } from '../../../managers/popUpManager';
 
 @Component({
   selector: 'solicitud-transferencia',
@@ -14,19 +16,24 @@ export class SolicitudTransferenciaComponent implements OnInit, OnDestroy, After
   formTransferencia: any;
   formRespuesta: any;
   sub = null;
+  nivelNombre: string;
   nivel: string;
   tipo: string;
   periodo = '2022-1';
   inscriptionSettings: any = null;
   process: string;
-  general= {
+  loading: boolean;
+  proyectosCurriculares: any[];
+  codigosEstudiante: any[];
+
+  general = {
     nombre: 'Fabián Sánchez',
-    codigo:'20102020088',
+    codigo: '20102020088',
     documento: '123546987'
   }
-  responde= {
+  responde = {
     nombre: 'Fabián Sánchez',
-    rol:'administrativo',
+    rol: 'administrativo',
   }
 
   file = {
@@ -37,6 +44,8 @@ export class SolicitudTransferenciaComponent implements OnInit, OnDestroy, After
   constructor(
     private translate: TranslateService,
     private utilidades: UtilidadesService,
+    private sgaMidService: SgaMidService,
+    private popUpManager: PopUpManager,
     private location: Location,
     private _Activatedroute: ActivatedRoute
   ) {
@@ -52,32 +61,83 @@ export class SolicitudTransferenciaComponent implements OnInit, OnDestroy, After
 
   ngOnInit() {
     this.sub = this._Activatedroute.paramMap.subscribe((params: any) => {
-      const { type, level, process } = params.params;
-      this.nivel = atob(level);
-      this.tipo = atob(type);
+      const { id, process } = params.params;
       this.process = atob(process);
-      if(this.process === 'all') {
+
+      this.loading = true;
+      this.sgaMidService.get('transferencia/inscripcion/' + atob(id)).subscribe(inscripcion => {
+        if (inscripcion !== null) {
+          const origen = this.getIndexForm("ProgramaOrigen");
+          const origenExterno = this.getIndexForm('ProgramaOrigenInput');
+          const estudiante = this.getIndexForm("CodigoEstudiante");
+          const estudianteExterno = this.getIndexForm("CodigoEstudianteExterno");
+          const destino = this.getIndexForm("ProgramaDestino");
+
+          this.nivelNombre = inscripcion['Data']['Nivel']['Nombre'];
+          this.nivel = inscripcion['Data']['Nivel']['Id'];
+          this.tipo = inscripcion['Data']['TipoInscripcion']['Nombre'];
+          this.formTransferencia.campos[destino].valor = inscripcion['Data']['ProgramaDestino'];
+
+          if (this.tipo === "Transferencia externa") {
+            this.ocultarCampo(estudianteExterno, false);
+            this.ocultarCampo(estudiante, true);
+
+            this.ocultarCampo(origenExterno, false);
+            this.ocultarCampo(origen, true);
+          } else {
+            this.ocultarCampo(estudianteExterno, true);
+            this.ocultarCampo(estudiante, false);
+
+            this.ocultarCampo(origenExterno, true);
+            this.ocultarCampo(origen, false);
+
+            this.formTransferencia.campos[estudiante].deshabilitar = false;
+            this.formTransferencia.campos[estudiante].opciones = inscripcion['Data']['CodigoEstudiante'];
+
+            this.formTransferencia.campos[origen].deshabilitar = false;
+            this.formTransferencia.campos[origen].opciones = inscripcion['Data']['ProyectoCurricular'];
+            this.formTransferencia.campos[origen].opciones = inscripcion['Data']['ProyectoCodigo'];
+
+            if (this.tipo === "Reingreso") {
+
+              inscripcion['Data']['CodigoEstudiante'].forEach(codigo => {
+                if (codigo.IdProyecto === inscripcion['Data']['ProgramaDestino']['Id']) {
+                  this.formTransferencia.campos[estudiante].valor = codigo;
+                  this.formTransferencia.campos[estudiante].deshabilitar = true;
+                }
+              });
+
+              this.formTransferencia.campos[origen].valor = inscripcion['Data']['ProgramaDestino'];
+              this.formTransferencia.campos[origen].deshabilitar = true;
+            }
+          }
+          this.loading = false;
+        }
+
+      });
+
+      if (this.process === 'all') {
         this.formTransferencia.campos.forEach(element => {
-          element.deshabilitar = true; 
-          if(element.etiqueta === 'file'){
+          element.deshabilitar = true;
+          if (element.etiqueta === 'file') {
             element.ocultar = true;
           }
         });
         this.formTransferencia.btn = false;
       }
-      this.inscriptionSettings = this.nivel === 'Pregrado' ? {
+      this.inscriptionSettings = this.nivelNombre === 'Pregrado' ? {
         basic_info_button: true,
         hide_header_labels: true,
         formacion_academica_button: true,
         documentos_programa_button: true,
-        nivel: this.nivel,
+        nivel: this.nivelNombre,
         detalle_inscripcion: true,
       } : {
         basic_info_button: true,
         hide_header_labels: true,
         formacion_academica_button: true,
         documentos_programa_button: true,
-        nivel: this.nivel,
+          nivel: this.nivelNombre,
         detalle_inscripcion: true,
         experiencia_laboral: true,
         produccion_academica: true,
@@ -86,7 +146,26 @@ export class SolicitudTransferenciaComponent implements OnInit, OnDestroy, After
     })
   }
 
-  ngAfterViewInit(){
+  ocultarCampo(campo, ocultar) {
+    this.formTransferencia.campos[campo].ocultar = ocultar;
+    this.formTransferencia.campos[campo].requerido = !ocultar;
+  }
+
+  getIndexForm(nombre: String): number {
+    for (let index = 0; index < this.formTransferencia.campos.length; index++) {
+      const element = this.formTransferencia.campos[index];
+      if (element.nombre === nombre) {
+        return index
+      }
+    }
+    return 0;
+  }
+
+  loadSolicitud() {
+
+  }
+
+  ngAfterViewInit() {
 
   }
 
@@ -102,7 +181,7 @@ export class SolicitudTransferenciaComponent implements OnInit, OnDestroy, After
 
   }
 
-  respuestaForm(event){
+  respuestaForm(event) {
     console.log(event);
   }
 
@@ -110,8 +189,8 @@ export class SolicitudTransferenciaComponent implements OnInit, OnDestroy, After
     console.log(event);
   }
 
-  validarFormRespuesta(event){
+  validarFormRespuesta(event) {
     console.log(event);
-    
+
   }
 }
