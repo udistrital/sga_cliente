da554c26d55c98f0e59cff38dc9811e8350cbedb
nefawaltpint <nefawaltpint@gmail.com>
2022-06-15 17:15:55 -0500

    fix: ajuste porcentaje tab documentos solicitados.

--- src/app/pages/inscripcion/inscripcion_general/inscripcion_general.component.ts
+++ src/app/pages/inscripcion/inscripcion_general/inscripcion_general.component.ts
@@ -604,17 +604,17 @@ export class InscripcionGeneralComponent implements OnInit, OnChanges {
                   response => {
 
                     if (response.Metadatos === '') {
-                      this.percentage_docu += Math.round((1 / this.tipo_documentos.length * 100) * 100) / 100;
+                      this.percentage_docu += (1 / this.tipo_documentos.length * 100);
                     } else {
                       if (response.Metadatos !== '') {
                         if (JSON.parse(response.Metadatos).aprobado) {
-                          this.percentage_docu += Math.round((1 / this.tipo_documentos.length * 100) * 100) / 100;
+                          this.percentage_docu += (1 / this.tipo_documentos.length * 100);
                         }
                       }
                     }
-                    this.percentage_docu = Number(this.percentage_docu.toFixed(0));
+                    this.percentage_docu = Math.round(this.percentage_docu);
 
-                    if (this.percentage_docu > 100) {
+                    if (this.percentage_docu >= 98) {
                       this.percentage_docu = 100;
                     }
 
