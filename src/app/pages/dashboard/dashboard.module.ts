1943378296e71d20433614ca09bac9cc4e0b58b8
lud <ludwinggsanchez@gmail.com>
2023-04-21 12:31:14 -0500

    feat #1232: Guards
    - Limpieza de dependencias.
    - :host /deep/ -> ::deep
    - Actualización imports de rxjs.

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 
-import { NgxEchartsModule } from 'ngx-echarts';
-
 import { ThemeModule } from '../../@theme/theme.module';
 import { DashboardComponent } from './dashboard.component';
 import { CarouselComponent } from './carousel/carousel.component';
@@ -11,7 +9,6 @@ import { NewsComponent } from './news/news.component';
 @NgModule({
   imports: [
     ThemeModule,
-    NgxEchartsModule,
   ],
   declarations: [
     DashboardComponent,
