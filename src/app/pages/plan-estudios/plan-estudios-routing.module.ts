1971141de033e26be5d8e81c4b1af9b721a00698
edwargl7 <07edwargl@gmail.com>
2023-10-25 22:16:48 -0500

    feat: udistrital/sga_cliente#1457 Ajuste plan estudios routing

--- src/app/pages/plan-estudios/plan-estudios-routing.module.ts
+++ src/app/pages/plan-estudios/plan-estudios-routing.module.ts
@@ -43,7 +43,7 @@ export const routedComponents = [
   EdicionPlanEstudiosComponent,
   SummaryPlanesEstudioComponent,
   SafeURL,
-  VisualizarDocumentoPlanComponent,,
+  VisualizarDocumentoPlanComponent,
   EvaluarPlanEstudiosComponent,
   DialogoEvaluarComponent
 ]
