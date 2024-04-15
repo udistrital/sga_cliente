a4a27e4ccd695e70deea965aead2394e2258f464
Mafe Avendaño <mafe.ud@gmail.com>
2024-04-15 15:45:18 -0500

    Update environment.test.ts

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -13,7 +13,7 @@ export const environment = {
       PASS: 'xxx',
     },
   },
-  SECRET_KEY: $SECRET_KEY,
+  SECRET_KEY: '',
   NUXEO_SERVICE:'https://autenticacion.portaloas.udistrital.edu.co/apioas/gestor_documental_mid/v1',
   CONFIGURACION_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/configuracion_crud_api/v1/',
   NOTIFICACION_SERVICE: 'wss://pruebasapi.portaloas.udistrital.edu.co:8116/ws',
