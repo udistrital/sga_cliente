77cb9f74bd5e67873f8b759d613cee970fa6dd31
diegofceliv <fernando.dfcv@gmail.com>
2024-06-07 01:01:03 -0500

    feat: Se actualizan environments

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -67,6 +67,5 @@ export const environment = {
   GOOGLE_MID_SERVICE: 'http://pruebasapi.intranetoas.udistrital.edu.co:8514/v1/',
   ESPACIOS_ACADEMICOS_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/espacios_academicos_crud/v1/',
   PLAN_TRABAJO_DOCENTE_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/plan_trabajo_docente_crud/v1/',
-  PLAN_ESTUDIOS_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/planes_estudios_crud/v1/',
-  SGA_MID_TERCERO: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/terceros_mid/v2/',
+  DEPENDENCIAS_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/dependencias_api/v1/',
 };
