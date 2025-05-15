b4a4727d50179dec913cdc862225239a720430ac
WillianRu <correo.willianrueda@gmail.com>
2025-05-15 16:16:04 -0500

    feat #1591 Validador de numero de documento y delimitacion de campos obligatorios con *

--- src/app/pages/admision/dialogo-formulario-pagador/dialogo-formulario-pagador.component.ts
+++ src/app/pages/admision/dialogo-formulario-pagador/dialogo-formulario-pagador.component.ts
@@ -340,7 +340,7 @@ export class DialogoFormularioPagadorComponent implements OnInit, OnDestroy {
       aprobado: [false, Validators.required],
       naturaleza: ['', Validators.required],
       tipoDocumento: ['', Validators.required],
-      numeroDocumento: ['', [Validators.required, Validators.pattern(/^[^.,]+$/)]],
+      numeroDocumento: ['', [Validators.required, Validators.pattern(/^[0-9]{8,14}$/)]],
       primerNombre: ['', Validators.required],
       segundoNombre: [''],
       primerApellido: ['', Validators.required],
