6a71ae7014e9287620e60e68b9011a5cbf150fe7
Stiven Osorio <jsosoriov@udistrital.edu.co>
2025-10-20 10:03:05 -0500

    fix: actualizacion de sga_cliente /udistrital/sga_documentacion#628
    Se cambia mensaje de error al momento de hacer una nueva solicitud fuera de las fechas establecidas tanto en Inscripciones como en transferencias
    
    Se muestra el mensaje: **No puede inscribirse, el periodo de inscripciones ha finalizado.**

--- src/app/pages/inscripcion/crud-inscripcion_multiple/crud-inscripcion_multiple.component.ts
+++ src/app/pages/inscripcion/crud-inscripcion_multiple/crud-inscripcion_multiple.component.ts
@@ -487,7 +487,7 @@ export class CrudInscripcionMultipleComponent implements OnInit {
     if (fechaActual.isAfter(fechaLimite, 'day')) {
       Swal.fire({
         icon: 'warning',
-        text: this.translate.instant('calendario.sin_proyecto_curricular'),
+        text: this.translate.instant('inscripcion.no_hay_programa_evento'),
         confirmButtonText: this.translate.instant('GLOBAL.aceptar'),
       });
       return;
