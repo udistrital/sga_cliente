4af9342be3f5f923cbe87b754955cf0296355470
Stiven Osorio <jsosoriov@udistrital.edu.co>
2025-11-24 10:02:44 -0500

    Visualización de roles en sga_cliente /udistrital/sga_documentacion/issues/651
    Ajustes finales de visualización de rol en componente de selección.
    
    Se agrega un valor inicial por defecto y una etiqueta **Roles** para indicar al usuario.

--- src/app/@theme/components/header/header.component.ts
+++ src/app/@theme/components/header/header.component.ts
@@ -63,46 +63,9 @@ export class HeaderComponent {
       }
       const roles_unicos = [...(new Set(roles))];
 
-      // this.roles_sga = String(roles_unicos).replace(/,/g, ', ');
       this.roles_sga = roles_unicos;
-      // this.rolSeleccionado = String(roles_unicos[0]);
-      // const listaRoles = [
-      //   "ADMIN_SGA","ADMIN_CAMPUS",
-      //   "VICERRECTOR", "ASESOR_VICE",
-      //   "COORDINADOR", "ASIS_PROYECTO",
-      //   "DOCENTE", "ESTUDIANTE", "ASPIRANTE",
-      //   "SOPORTE_OAS", "SOPORTE_NIVEL2",
-      //   "PROVEEDOR", "CONTRATISTA",
-      //   "ABOGADO_CONTRATACION_RECTOR",
-      //   "JEFE_CONTROL_INTERNO",
-      //   "SECRETARIO_AUDITOR",
-      //   "AUDITOR_EXPERTO",
-      //   "ADMISIONES_REG"
-      // ];
-      // let imp_r = [];
-      // roles_unicos.forEach((rol: string)=> {
-      //   imp_r.push( listaRoles.indexOf(rol) );
-      // });
-      // // console.log(roles_unicos);
-      // // console.log(imp_r);
-      // let min_rol = [];
-      // if (imp_r.length > 3){
-      //   for (let i = 0; i < 3; i++) {
-      //     let m = Math.min(...imp_r);
-      //     min_rol.push( m );
-      //     let rol_aux = imp_r.filter(num => num !== min_rol[i]);
-      //     imp_r = rol_aux;
-      //   }
-      // } else {
-      //   min_rol = imp_r;
-      // }
+      this.rolSeleccionado = String(roles_unicos[0]);
 
-      // let roles_finales = [];
-      // for (let i = 0; i < min_rol.length; i++) {
-      //   roles_finales.push( listaRoles[ min_rol[i] ] );
-      // }
-      
-      // this.roles_sga = String(roles_finales).replace(/,/g, ', ');
       this.username = typeof user.email !== 'undefined' ? user.email : typeof userService.email !== 'undefined' ? userService.email : '';
       this.liveTokenValue = this.username !== '';
     })
